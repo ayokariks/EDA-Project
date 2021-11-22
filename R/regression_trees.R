5b8082ba81715e00406bfde26f47943ad4a59365
Tosin Onaolapo <83357004+tosinwashere@users.noreply.github.com>
2021-11-22 16:55:55 +0000

    sorry for all the commits guys i'm just getting the hang of this and don't want to forget lol

--- R/regression_trees.R
+++ R/regression_trees.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(rpart) # for regression trees
 library(caret) # for splitting into train and test sets
 
-df <- read_csv("sKor_data_tot_v01")
+df <- read_csv("../data/sKor_data_tot_v01.csv")
 
 set.seed(123)
 train_index <- createDataPartition(df$X1, times = 1, p = 0.7, list = FALSE)
