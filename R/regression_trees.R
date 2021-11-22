2cd227fec92be60e76f23844ad7a7a46bfe6ed5c
Tosin Onaolapo <83357004+tosinwashere@users.noreply.github.com>
2021-11-22 16:49:55 +0000

    Update regression_trees.R

--- R/regression_trees.R
+++ R/regression_trees.R
@@ -5,17 +5,10 @@ library(tidyverse)
 library(rpart) # for regression trees
 library(caret) # for splitting into train and test sets
 
-df <- read_csv("../data/sKor_data_v01.csv")
+df <- read_csv("sKor_data_tot_v01")
 
-#set.seed(123)
+set.seed(123)
 train_index <- createDataPartition(df$X1, times = 1, p = 0.7, list = FALSE)
 train <- df %>% slice(train_index)
 test <- df %>% slice(-train_index)
 
-
-fit <- rpart(num_tot_energy_heat ~ ., data = df)
-
-qplot(num_tot_energy_heat, num_hus_age, data = df)
-
-plot(fit, margin = 0.1)
-text(fit, cex = 0.75)
