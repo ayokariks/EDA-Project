4c67eb4c9e37a2aaf1e8f4600e6eeb8572943b1e
Tosin Onaolapo <83357004+tosinwashere@users.noreply.github.com>
2021-11-29 13:40:33 +0000

    final tree + errors

--- R/regression_trees.R
+++ R/regression_trees.R
@@ -5,6 +5,7 @@ setwd("~/R/UCL/BENV0091/EDA-Project/EDA-Project/R")
 
 library(tidyverse)
 library(rpart) # for regression trees
+library(rpart.plot)
 library(caret) # for splitting into train and test sets
 
 df <- read_csv("../data/sKor_data_tot_v02.csv")
@@ -31,7 +32,6 @@ test <- df %>% slice(-train_index)
 #maxdepth - maximum depth of any node of the final tree (meaning?)
 #minbucket is automatically minsplit/3
 
-#cross validation to pick the best tuning parameters
 
 train_rpart <- train(num_tot_energy_heat ~ ., 
                      method = "rpart",
@@ -46,7 +46,7 @@ best_cp <- train_rpart$bestTune
 #currently = 0.0041666 [3] RMSE 4927.586 (after cross validation)
 
 #very rudimentary way of sourcing the row with the best cp but oh well
-train_rpart[["results"]][3,]
+errors <- train_rpart[["results"]][3,]
 
 y_hat <- predict(train_rpart, newdata = train, type = "raw")
 
@@ -55,68 +55,46 @@ y_hat <- predict(train_rpart, newdata = train, type = "raw")
 # but apparently not? caret_fit <- train_rpart$finalModel 
 # use full trian_rpart
 
+results <- train %>% transmute(
+  actual = num_tot_energy_heat,
+  predicted = y_hat,
+  error = actual-predicted,
+  mse = mean(error^2),
+  rmse = sqrt(mse),
+  mae = mean(actual-predicted)
+)
+
+#right now, very high errors lol
+#i've seen that the rmses/values when using rpart(), using train
+# method = rpart are different, and also manually calculating rmse
+#is different from the one given in the cp table?
+
+#plotting the decision tree
 
 plot(train_rpart$finalModel, margin = 0.1)
 text(train_rpart$finalModel, cex = 0.75)
 
-results <- train %>% transmute(
-  actual = num_tot_energy_heat,
-  predicted = y_hat,
-  error = actual-predicted
-)
+#using rpart plot gives a much better tree but low quality
+rpart.plot(train_rpart$finalModel, tweak = 1.8, # adjusts size from default
+           box.palette = "Purples",
+           type = 1,
+           fallen.leaves = FALSE,
+           extra = 0) # takes away % 
+# see https://cran.r-project.org/web/packages/rpart.plot/rpart.plot.pdf
+# pg 17 for more help with playing with rpart.plot
+
+#plotting errors
+results %>% ggplot(aes(actual,predicted)) + 
+  geom_point() +
+  geom_abline(intercept = 0, slope = 1) +
+  ylim(0, max(results$predicted)) +
+  xlim(0, max(results$actual))
+#abline isnt going through origin?
 
 
+## notes to self
+min(results$actual)
+min(results$predicted) #should we remove outliers?
 
-
-
-#creating a hypergrid that we can then use to tune rpart for minsplit
-#and maxdepth
-#honestly not sure if this is worth it, as this happens outside caret and 
-#cp is most important 
-
-hyper_grid <- expand.grid(
-  minsplit = seq(20, 60, 1),
-  maxdepth = seq(30, 60, 1)
-)
-
-#setting up a for loop to iterate through each minsplit and maxdepth
-#combination, creating a different model for each combo
-
-models <- list()
-
-for (i in 1:nrow(hyper_grid)) {
-  
-  # get minsplit, maxdepth values at row i
-  minsplit <- hyper_grid$minsplit[i]
-  maxdepth <- hyper_grid$maxdepth[i]
-  
-  # train a model and store in the list
-  models[[i]] <- rpart(
-    formula = num_tot_energy_heat ~ .,
-    data    = train,
-    control = list(minsplit = minsplit, maxdepth = maxdepth)
-  )
-}
-
-# function to get optimal cp from each model
-get_cp <- function(x) {
-  min    <- which.min(x$cptable[, "xerror"])
-  cp <- x$cptable[min, "CP"] 
-}
-
-# function to get minimum error
-get_min_error <- function(x) {
-  min    <- which.min(x$cptable[, "xerror"])
-  xerror <- x$cptable[min, "xerror"] 
-}
-
-#seeing best cps and errors alongside the minsplit and maxbucket values
-hyper_grid %>%
-  mutate(
-    cp    = purrr::map_dbl(models, get_cp),
-    error = purrr::map_dbl(models, get_min_error)
-  ) %>%
-  arrange(error) %>%
-  top_n(-5, wt = error)
-
-#right now, very high errors lol
+results %>% ggplot(aes(actual,error)) + 
+  geom_point()
